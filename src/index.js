5f40e6097cae37cf936e9d2fc762933ca8870239
Priyal Binu <priyalbinu@gmail.com>
2025-11-13 10:22:46 +0530

    Fixed ESLint import order and Amplify config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
-
-Amplify.configure(awsconfig);
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "react-oidc-context";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+// Configure Amplify **after** imports
+Amplify.configure(awsconfig);
 
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_9CwAB85xS",
@@ -17,6 +17,7 @@ const cognitoAuthConfig = {
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <AuthProvider {...cognitoAuthConfig}>
